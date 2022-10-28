52e87ea1abbd5c3deb09e2dcae964a1eb7f3cc35
Samonto Karmaker <samonto0726@gmail.com>
2022-10-28 21:34:44 +0600

    Added basic employee with data

---
+++ src/Components/Employee.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+import employee_data from "../Data/employeeData";
+import femaleProfile from "../Data/image/femaleProfile.jpg"
+import maleProfile from "../Data/image/maleProfile.jpg"
+
+const Employee = () => {
+
+    const [employees, setEmployees] = useState(employee_data)
+
+    return(
+        <div className="Container">
+            <div className="row">
+                <div className="col-2">
+                    {
+                        employees.map((employee) => (
+                                <div id={employee.id} className="card">
+                                    <img src={femaleProfile} className="card-img-top"/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Name: {employee.fullName}</h5>
+                                    </div>
+                                </div>
+                            )
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Employee
