4c1865355a53ed2864049fb7522bb885673f03e0
Samonto Karmaker <samonto0726@gmail.com>
2022-10-31 19:07:47 +0600

    Added Styling in the card

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import employee_data from "../Data/employeeData";
 import femaleProfile from "../Data/image/femaleProfile.jpg"
 import maleProfile from "../Data/image/maleProfile.jpg"
+import "../App.css"
 
 const Employee = () => {
 
@@ -11,18 +12,20 @@ const Employee = () => {
     return(
         <div className="Container">
             <div className="row justify-content-center mt-3 md-3">
-                <div className="col-2">
-                    {
-                        employees.map((employee) => (
-                                <div id={employee.id} className="card">
-                                    <img src={femaleProfile} className="card-img-top"/>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Name: {employee.fullName}</h5>
+                <div className="col-4">
+                    <div className="collection">
+                        {
+                            employees.map((employee) => (
+                                    <div id={employee.id} className="card m-2" style={{cursor: "pointer"}}>
+                                        <img src={femaleProfile} className="card-img-top"/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Name: {employee.fullName}</h5>
+                                        </div>
                                     </div>
-                                </div>
+                                )
                             )
-                        )
-                    }
+                        }
+                    </div>
                 </div>
             </div>
         </div>
