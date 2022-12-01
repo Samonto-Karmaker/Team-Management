b42038bc0ae69ee73096792f95b56101c9b41ef1
Samonto Karmaker <samonto0726@gmail.com>
2022-12-01 22:00:18 +0600

    Added appropriate footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+const date = new Date()
+
 const Footer = () => {
     return(
-        <div>
-
+        <div className="Container">
+            <div className="row justify-content-center mt-3 md-3">
+                <div className="col-6">
+                    <h5>Team Management App - {date.getFullYear()}</h5>
+                </div>
+            </div>
         </div>
     )
 }
