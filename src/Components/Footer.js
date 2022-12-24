89814050775aa43bb3701b116cac582ee189dfa4
Samonto Karmaker <samonto0726@gmail.com>
2022-12-24 22:14:19 +0600

    Added styling in the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../App.css"
 
 const date = new Date()
 
 const Footer = () => {
     return(
         <div className="Container">
-            <div className="row justify-content-center mt-3 md-3">
+            <div className="row justify-content-center mt-3 md-3" style={{"background-color": "rgba(44, 11, 194, 0.6)", "padding" : "10px"}}>
                 <div className="col-6">
                     <h5>Team Management App - {date.getFullYear()}</h5>
                 </div>
